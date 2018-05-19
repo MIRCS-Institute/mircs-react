ec1d26d6fbe78453f8c597f7157827f5867fb7d7
Shaun Johansen <shaun.johansen@eyeball.io>
2018-05-19 18:52:30 -0300

    create Map component from Maps page, zoom map to selected points using the amazing turfjs library for calculations

---
+++ react-app/src/components/Map.js
@@ -0,0 +1,190 @@
+import _ from 'lodash'
+import {action, autorun, extendObservable} from 'mobx'
+import {FormControl} from 'material-ui/Form'
+import {MenuItem} from 'material-ui/Menu'
+import {observer} from 'mobx-react'
+import http from 'utils/http'
+import Input, {InputLabel} from 'material-ui/Input'
+import Layout from 'utils/Layout'
+import PropTypes from 'prop-types'
+import React from 'react'
+import Select from 'material-ui/Select'
+import turf from 'turf'
+
+const L = window.L
+
+const Map = observer(class extends React.Component {
+  static propTypes = {
+    selected: PropTypes.object.isRequired,
+  }
+
+  constructor() {
+    super()
+    extendObservable(this, {
+      tileLayerName: 'Mapbox',
+      records: [],
+      points: [],
+    })
+  }
+
+  componentDidMount() {
+    this.startMap()
+    this.autorunDisposer = autorun(() => {
+      if (this.props.selected && this.props.selected.dataSetId) {
+        this.fetchDataSetForMap(this.props.selected.dataSetId)
+      }
+      if (this.props.selected && this.props.selected.relationId) {
+        this.fetchRelationshipDataForMap(this.props.selected.relationId)
+      }
+    })
+  }
+
+  componentWillUnmount() {
+    this.stopMap()
+    this.autorunDisposer()
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    this.setupTileLayer()
+  }
+
+  startMap = () => {
+    this.map = L.map(this.refs.mapNode, {
+      center: [
+        44.6458, -63.5778
+      ],
+      zoom: 13,
+    })
+
+    L.control.scale({position: 'bottomleft'}).addTo(this.map)
+
+    this.setupTileLayer()
+  }
+
+  fetchDataSetForMap = (dataSetId) => {
+    this.refreshMap()
+    this.fetchDataSet(`/api/datasets/${dataSetId}/records`, 'bodyJson.list')
+  }
+
+  fetchRelationshipDataForMap = (relationId) => {
+    this.refreshMap()
+    this.fetchDataSet(`/api/relationships/${relationId}/join`, 'bodyJson.records')
+  }
+
+  refreshMap = action(() => {
+    this.stopMap()
+    this.startMap()
+  })
+
+  createPoints = action((records) => {
+    const icon = L.icon({ iconUrl: 'house.svg' })
+    const points = []
+
+    _.each(records, (record) => {
+      const point = [record.Y || record.y || record.latitude, record.X || record.x || record.longitute]
+      if (point[0]) {
+        points.push(point)
+        L.marker(point, { icon })
+          .addTo(this.map)
+          .bindPopup(action(() => {
+            return _.map(record, (value, field) => (
+              `<strong>${field}:</strong> <span>${value}</span>`
+            )).join('<br>')
+          }))
+      }
+    })
+
+    // position map
+    const multiPoint = turf.multiPoint(points)
+    const bbox = turf.bbox(multiPoint)
+    this.map.fitBounds([[bbox[0], bbox[1]], [ bbox[2], bbox[3]]])
+
+    this.points = points
+  })
+
+  fetchDataSet = action((recordId, where) => {
+    http.jsonRequest(recordId)
+      .then(action((response) => {
+        this.records = _.get(response, where)
+        this.createPoints(this.records)
+      }))
+      .catch(action((error) => {
+        this.error = error
+      }))
+  })
+
+  stopMap = action(() => {
+    if (this.map) {
+      this.map.remove()
+      this.map = null
+    }
+  })
+
+  setupTileLayer = action(() => {
+    if (this.tileLayer) {
+      this.tileLayer.remove()
+      this.tileLayer = null
+    }
+
+    switch (this.tileLayerName) {
+      case 'Mapbox':
+        this.tileLayer = L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+          minZoom: 8,
+          attribution: 'Map data &copy; <a href="https://openstreetmap.org">OpenStreetMap</a> contributor' +
+              's, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imager' +
+              'y © <a href="https://mapbox.com">Mapbox</a>',
+          id: 'mapbox.streets',
+          accessToken: 'pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriIS' +
+              'LbB6B5aw'
+        })
+        break
+
+      case 'OpenStreetMap':
+      default:
+        this.tileLayer = L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {attribution: '&copy; <a href="https://osm.org/copyright">OpenStreetMap</a> contributors'})
+        break
+    }
+
+    this.tileLayer.addTo(this.map)
+  })
+
+  handleTileLayerNameChange = action((event) => {
+    this.tileLayerName = event.target.value
+  })
+
+  render() {
+    return (
+      <div style={{ ...Layout.column, flex: 1 }}>
+
+        <form autoComplete='off'>
+          <FormControl>
+            <InputLabel htmlFor='tile-layer-input'>Tile Layer</InputLabel>
+            <Select
+              value={this.tileLayerName}
+              onChange={this.handleTileLayerNameChange}
+              input={< Input id = 'tile-layer-input' />}>
+              <MenuItem value='OpenStreetMap'>OpenStreetMap</MenuItem>
+              <MenuItem value='Mapbox'>Mapbox</MenuItem>
+            </Select>
+          </FormControl>
+        </form>
+
+        <div ref='mapNode' style={styles.map}></div>
+
+        <center>
+          {this.points.length} properties
+        </center>
+
+      </div>
+    )
+  }
+})
+
+const styles = {
+  map: {
+    position: 'relative',
+    flex: 1,
+  },
+}
+
+export default Map
--- react-app/src/pages/Maps.js
+++ react-app/src/pages/Maps.js
@@ -1,114 +1,34 @@
 import _ from 'lodash'
 import {action, extendObservable} from 'mobx'
-import {FormControl} from 'material-ui/Form'
-import {MenuItem} from 'material-ui/Menu'
 import {observer} from 'mobx-react'
 import Button from 'material-ui/Button'
 import Card, {CardContent, CardHeader} from 'material-ui/Card'
 import http from 'utils/http'
-import Input, {InputLabel} from 'material-ui/Input'
 import Layout from 'utils/Layout'
+import Map from 'components/Map'
 import React from 'react'
-import Select from 'material-ui/Select'
-
-const L = window.L;
 
 const Maps = observer(class extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {
-      tileLayerName: 'OpenStreetMap'
-    };
     extendObservable(this, {
       dataSets: [],
       relationships: [],
-      records: [],
-      selectedRecord: null
+      selected: {},
     });
   }
 
   componentDidMount() {
     this.fetchDataSetRecords();
-    this.startMap();
+    this.fetchRelationshipRecords();
   }
-
-  listMappableDataSets() {}
-
-  componentWillUnmount() {
-    this.stopMap();
-  }
-  // create the Leaflet map object
-  startMap = action(() => {
-    this.map = L.map(this.refs.mapNode, {
-      center: [
-        44.6458, -63.5778
-      ],
-      zoomControl: false,
-      zoom: 13,
-      maxZoom: 18,
-      minZoom: 9,
-      scrollwheel: true,
-      legends: false,
-      infoControl: false,
-      attributionControl: true
-    });
-
-    L.control.zoom({position: 'bottomleft'}).addTo(this.map);
-    L.control.scale({position: 'bottomleft'}).addTo(this.map);
-
-    this.setupTileLayer();
-  })
-
-  // this is called when 'map data' is clicked
-  fetchDataSetForMap = action((dataSetId) => {
-    this.refreshMap();
-    this.fetchDataSet(`/api/datasets/${dataSetId}/records`, 'bodyJson.list');
-  })
-
-  fetchRelationshipDataForMap = action((relationId) => {
-    this.refreshMap();
-    this.fetchDataSet(`/api/relationships/${relationId}/join`, 'bodyJson.records');
-  })
-
-  // once a new data set is fetched, we want to remove the old one from the map
-  refreshMap = action(() => {
-    this.stopMap();
-    this.startMap();
-  })
-
-  // loop through the records to map each set of data
-  createPoints = action((records) => {
-    const icon = L.icon({ iconUrl: 'house.svg' })
-    _.each(records, (record) => {
-      L.marker([record.Y || record.y || record.latitude, record.X || record.x || record.longitute], { icon })
-        .addTo(this.map)
-        .bindPopup(record.Address_Number + ' ' + record.Street)
-        .on('click', action(() => {
-          this.selectedRecord = record;
-        }));
-    });
-  })
-
-  fetchDataSet = action((recordId, where) => {
-    this.selectedRecord = null;
-    http.jsonRequest(recordId)
-      .then(action((response) => {
-        this.records = _.get(response, where);
-        this.createPoints(this.records);
-      }))
-      .catch(action((error) => {
-        this.error = error;
-      }));
-  })
 
   // use this to list all of the datasets on the left
   // TODO make sure we only request "mappable" datasets
   fetchDataSetRecords = action(() => {
-    this.selectedRecord = null;
     http.jsonRequest('/api/datasets')
       .then(action((response) => {
         this.dataSets = _.get(response, 'bodyJson.list');
-        this.fetchRelationshipRecords();
       }))
       .catch(action((error) => {
         this.error = error;
@@ -124,50 +44,6 @@ const Maps = observer(class extends React.Component {
         this.error = error;
       }));
   })
-
-  stopMap = action(() => {
-    if (this.map) {
-      // destroy the Leaflet map object & related event listeners
-      this.map.remove();
-      this.map = null;
-    }
-  })
-
-  setupTileLayer = action(() => {
-    if (this.tileLayer) {
-      this.tileLayer.remove();
-      this.tileLayer = null;
-    }
-
-    switch (this.state.tileLayerName) {
-      case 'Mapbox':
-        this.tileLayer = L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
-          minZoom: 8,
-          attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributor' +
-              's, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imager' +
-              'y © <a href="http://mapbox.com">Mapbox</a>',
-          id: 'mapbox.streets',
-          accessToken: 'pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriIS' +
-              'LbB6B5aw'
-        });
-        break;
-
-      case 'OpenStreetMap':
-      default:
-        this.tileLayer = L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'});
-        break;
-    }
-
-    this.tileLayer.addTo(this.map);
-  })
-
-  componentDidUpdate(prevProps, prevState) {
-    this.setupTileLayer();
-  }
-
-  handleTileLayerNameChange = event => {
-    this.setState({tileLayerName: event.target.value});
-  };
 
   render() {
     return (
@@ -210,9 +86,9 @@ const Maps = observer(class extends React.Component {
                         value={dataSet._id}
                         variant='raised'
                         color='primary'
-                        onClick={() => {
-                        this.fetchDataSetForMap(dataSet._id)
-                      }}>Map Data</Button>
+                        onClick={action(() => {
+                          this.selected = { dataSetId: dataSet._id }
+                        })}>Map Data</Button>
                     </div>
                   </CardContent>
                 </Card>
@@ -255,9 +131,9 @@ const Maps = observer(class extends React.Component {
                         value={relation._id}
                         variant='raised'
                         color='primary'
-                        onClick={() => {
-                        this.fetchRelationshipDataForMap(relation._id)
-                      }}>Map Data</Button>
+                        onClick={action(() => {
+                          this.selected = { relationId: relation._id }
+                        })}>Map Data</Button>
                     </div>
                   </CardContent>
                 </Card>
@@ -266,41 +142,13 @@ const Maps = observer(class extends React.Component {
 
         </div>
 
-        <div style={{ ...Layout.column, flex: 1 }}>
+        <Map selected={this.selected}/>
 
-          <form autoComplete='off'>
-            <FormControl>
-              <InputLabel htmlFor='tile-layer-input'>Tile Layer</InputLabel>
-              <Select
-                value={this.state.tileLayerName}
-                onChange={this.handleTileLayerNameChange}
-                input={< Input id = 'tile-layer-input' />}>
-                <MenuItem value='OpenStreetMap'>OpenStreetMap</MenuItem>
-                <MenuItem value='Mapbox'>Mapbox</MenuItem>
-              </Select>
-            </FormControl>
-          </form>
-
-          <div ref='mapNode' style={styles.map}></div>
-
-          <div style={{ overflow: 'scroll' }}>
-            {_.map(this.selectedRecord, (value, field) => (
-              <div key={field}>
-                <strong>{field}:</strong> <span>{value}</span>
-              </div>
-            ))}
-          </div>
-
-        </div>
       </div>
     );
   }
 });
 const styles = {
-  map: {
-    position: 'relative',
-    height: '700px'
-  },
   button: {
     margin: "auto",
     marginTop: "10px",
@@ -308,7 +156,7 @@ const styles = {
   },
   card: {
     margin: "10px"
-  }
+  },
 }
 
 export default Maps;
