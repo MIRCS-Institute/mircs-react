c78b2b4e94006b25d6341e25035ba610d024039a
Shaun Johansen <shaunjohansen@gmail.com>
2019-02-02 21:02:50 -0400

    bug fixing in decode-bearer-token.middleware

--- server/src/api/middleware/decode-bearer-token.middleware.js
+++ server/src/api/middleware/decode-bearer-token.middleware.js
@@ -7,24 +7,29 @@ const JWT_SECRET_KEY = Environment.getRequired('JWT_SECRET_KEY')
 
 const decodeBearerToken = (req, res, next) => {
   const authorization = req.headers.authorization
-  if (authorization && _.startsWith(authorization.toLowerCase(), 'bearer ')) {
-    try {
-      const token = authorization.split(' ')[1]
-      if (!token) {
-        return next(HttpErrors.badRequest400('missing token'))
-      }
+  if (!authorization) {
+    return next()
+  }
+  if (!_.startsWith(authorization.toLowerCase(), 'bearer ')) {
+    return next(HttpErrors.badRequest400('unsupported authorization'))
+  }
+  const token = authorization.split(' ')[1]
+  if (!token) {
+    return next(HttpErrors.badRequest400('missing token'))
+  }
 
-      const decoded = jwt.verify(token, JWT_SECRET_KEY)
-      req.authentication = decoded
-
-    } catch (exception) {
-      if (exception.name === 'TokenExpiredError') {
-        console.error('TokenExpiredError')
-      } else {
-        console.error(exception)
-      }
+  try {
+    const decoded = jwt.verify(token, JWT_SECRET_KEY)
+    req.authentication = decoded
+  } catch (exception) {
+    if (exception.name === 'TokenExpiredError') {
+      return next(HttpErrors.badRequest401('token expired'))
+    } else {
+      return next(exception)
     }
   }
+
+  next()
 }
 
 module.exports = decodeBearerToken
