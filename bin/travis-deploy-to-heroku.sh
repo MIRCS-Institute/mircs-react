10be674b638cb92f9bd9a1161da1d80a5594b69f
Shaun Johansen <shaunjohansen@gmail.com>
2019-02-10 21:44:58 -0400

    use fetch instead of pull

--- bin/travis-deploy-to-heroku.sh
+++ bin/travis-deploy-to-heroku.sh
@@ -17,7 +17,11 @@ git init
 git add .
 git commit -m "Travis Build - Commit:${TRAVIS_COMMIT} ${TRAVIS_COMMIT_MESSAGE}"
 
+echo 'git remote add heroku https://:${HEROKU_API_KEY}@git.heroku.com/${HEROKU_APP_NAME}.git'
 git remote add heroku https://:${HEROKU_API_KEY}@git.heroku.com/${HEROKU_APP_NAME}.git
-git pull heroku master
+echo 'git fetch heroku'
+git fetch heroku
+echo 'git rebase heroku/master'
 git rebase heroku/master
+echo 'git push heroku master'
 git push heroku master
