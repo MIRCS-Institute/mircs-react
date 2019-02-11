f91932b74859b894b6a3d6f9f130552594d0d295
Shaun Johansen <shaunjohansen@gmail.com>
2019-02-10 21:17:54 -0400

    trying a different tactic

--- bin/travis-deploy-to-heroku.sh
+++ bin/travis-deploy-to-heroku.sh
@@ -9,29 +9,12 @@ cd $__dirname/..
 
 HEROKU_APP_NAME=$1
 
-echo HEROKU_API_KEY: ${HEROKU_API_KEY}
-echo 'heroku auth:whoami'
-heroku auth:whoami
-echo 'heroku auth:login'
-heroku auth:login
-echo 'heroku auth:whoami'
-heroku auth:whoami
-# heroku config:set HEROKU_API_KEY=${HEROKU_API_KEY}
-
-echo 'heroku git:clone -a ${HEROKU_APP_NAME}'
-heroku git:clone -a ${HEROKU_APP_NAME}
-
 ./bin/prepare-deploy.sh
 
-# convert the build folder into a Heroku git folder
-mv ${HEROKU_APP_NAME}/.git build
-
 cd build
-if [ -z "$(git status --porcelain)" ]; then
-  echo "build directory clean is clean, skipping Heroku deploy."
-else
-  # Uncommitted changes
-  git add .
-  git commit -m "Travis Build - Commit:${TRAVIS_COMMIT} ${TRAVIS_COMMIT_MESSAGE}"
-  git push heroku master
-fi
+
+git init
+heroku git:remote -a ${HEROKU_APP_NAME}
+git add .
+git commit -m "Travis Build - Commit:${TRAVIS_COMMIT} ${TRAVIS_COMMIT_MESSAGE}"
+git push heroku master
