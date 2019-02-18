ee78e39f637c1e3d7f251cbc1535486e0ab52330
Shaun Johansen <shaunjohansen@gmail.com>
2019-02-18 14:47:15 -0400

    replace inline resource fetching in Home with api/resources library

--- react-app/src/pages/Home.js
+++ react-app/src/pages/Home.js
@@ -1,8 +1,6 @@
-import {action, extendObservable} from 'mobx'
+import { cachedServerHttpResource } from '../api/resources/ServerHttpResource'
 import { goToPath, Path } from '../app/App'
 import {observer} from 'mobx-react'
-import { showSnackbarMessage } from '../components/SnackbarMessages'
-import _ from 'lodash'
 import Button from '@material-ui/core/Button'
 import Card from '@material-ui/core/Card'
 import CardContent from '@material-ui/core/CardContent'
@@ -10,44 +8,16 @@ import CardHeader from '@material-ui/core/CardHeader'
 import Grid from '@material-ui/core/Grid'
 import PageSkeleton from '../components/PageSkeleton'
 import React from 'react'
-import ServerHttpApi from '../api/net/ServerHttpApi'
 import Typography from '@material-ui/core/Typography'
 
 const Home = observer(class extends React.Component {
-  constructor(props) {
-    super(props)
-    extendObservable(this, {
-      dataSets: [],
-      relationships: [],
-      selected: {},
-    })
-  }
-
-  componentDidMount() {
-    this.fetchDataSetRecords()
-    this.fetchRelationshipRecords()
-  }
-
-  fetchDataSetRecords = action(() => {
-    ServerHttpApi.jsonGet('/api/datasets')
-      .then(action((response) => {
-        this.dataSets = _.get(response, 'bodyJson.list')
-      }))
-      .catch(showSnackbarMessage)
-  })
-
-  fetchRelationshipRecords = action(() => {
-    ServerHttpApi.jsonGet('/api/relationships')
-      .then(action((response) => {
-        this.relationships = _.get(response, 'bodyJson.list')
-      }))
-      .catch(showSnackbarMessage)
-  })
-
   render() {
+    const dataSets = cachedServerHttpResource('/api/datasets').get('list', [])
+    const relationships = cachedServerHttpResource('/api/relationships').get('list', [])
+
     return (<PageSkeleton>
       <Grid container spacing={16}>
-        {this.dataSets.map((dataSet) => (
+        {dataSets.map((dataSet) => (
           <Grid key={dataSet._id} item xs={6} md={4}>
             <Card style={styles.card}>
               <CardContent>
@@ -75,7 +45,7 @@ const Home = observer(class extends React.Component {
           </Grid>
         ))}
 
-        {this.relationships.map((relation) => (
+        {relationships.map((relation) => (
           <Grid key={relation._id} item xs={6} md={4}>
             <Card style={styles.card}>
               <CardHeader title={relation.name}/>
