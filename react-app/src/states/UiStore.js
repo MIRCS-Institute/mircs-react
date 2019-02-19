eb38c8735e3c87a5ec93763c993c9ab984cfa7fd
Shaun Johansen <shaunjohansen@gmail.com>
2019-02-18 22:07:37 -0400

    improve spacing in UIStore

--- react-app/src/states/UiStore.js
+++ react-app/src/states/UiStore.js
@@ -3,13 +3,26 @@ import {extendObservable} from 'mobx'
 class UiStore {
   constructor() {
     extendObservable(this, {
-      tileLayerName: 'Mapbox', // The name of the tile layer to use.
-      searchStrings: [], // An array of strings that are being searched for
-      fieldNames: [], // An array of the field names returned in the records
-      highlightField: 'none', // The name of the field selected for highlighting
-      foundRecords: [], // An array of arrays of records, each corresponding to the searchString at the same index
-      points: [], // An array of points, each of which is a two value array of lat/long coordinates. ie [ [45, 126], [47, 123] ]
-      selected: {}, // An object containing the point and array of selected records: { point: [123,45], records: [...] }
+      // The name of the tile layer to use.
+      tileLayerName: 'Mapbox',
+
+      // An array of strings that are being searched for
+      searchStrings: [],
+
+      // An array of the field names returned in the records
+      fieldNames: [],
+
+      // The name of the field selected for highlighting
+      highlightField: 'none',
+
+      // An array of arrays of records, each corresponding to the searchString at the same index
+      foundRecords: [],
+
+      // An array of points, each of which is a two value array of lat/long coordinates. ie [ [45, 126], [47, 123] ]
+      points: [],
+
+      // An object containing the point and array of selected records: { point: [123,45], records: [...] }
+      selected: {},
     })
   }
 }
