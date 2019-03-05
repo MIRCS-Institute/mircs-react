8afe609fc9cb0abd98d11c7204cd54725efab407
Shaun Johansen <shaunjohansen@gmail.com>
2019-03-04 23:19:52 -0400

    implement View by copying contents of DataSetMap

--- react-app/src/pages/View/View.js
+++ react-app/src/pages/View/View.js
@@ -1,16 +1,25 @@
 import { CurrentView } from '../../api/View'
+import { getCurrentDataSetId } from '../../api/DataSet'
 import {observer} from 'mobx-react'
-import DataSetName from '../../components/DataSetName'
 import Layout from '../../utils/Layout'
+import Map from '../../components/Map'
+import MapDrawer from '../../components/MapDrawer'
+import MapFilter from '../../components/MapFilter'
+import MapStatus from '../../components/MapStatus'
 import PageSkeleton from '../../components/PageSkeleton'
 import React from 'react'
 
 const View = observer(class extends React.Component {
   render() {
+    const dataSetId = getCurrentDataSetId()
+
     return <PageSkeleton title={CurrentView.res.get('name')}>
       <div style={{ ...Layout.absoluteFill, ...Layout.row }}>
+        <MapDrawer />
         <div style={{ ...Layout.column, flex: 1 }}>
-          <DataSetName dataSetId={CurrentView.res.get('dataSetId')}/>
+          <MapFilter />
+          <Map selected={{ dataSetId }}/>
+          <MapStatus />
         </div>
       </div>
     </PageSkeleton>
