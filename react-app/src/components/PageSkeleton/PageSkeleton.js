c4917c48a643c1ab5630d88b73a3022cc1bc72a5
Dylan Smith <dylanett@gmail.com>
2019-10-27 15:38:47 -0300

    Remove MIRCS logo from the maps page (causing confusion between the prototype and the MIRCS home page)

--- react-app/src/components/PageSkeleton/PageSkeleton.js
+++ react-app/src/components/PageSkeleton/PageSkeleton.js
@@ -3,12 +3,13 @@ import { observer } from 'mobx-react'
 import { Path } from '../../app/App'
 import AppBar from '@material-ui/core/AppBar'
 import AuthMenu from './AuthMenu'
+import HomeIcon from '@material-ui/icons/Home'
 import Layout from '../../utils/Layout'
-import MircsLogo from '../../resources/mircs-logo-white.svg'
 import PropTypes from 'prop-types'
 import React from 'react'
 import SignedInUser from '../../states/SignedInUser'
 import Toolbar from '@material-ui/core/Toolbar'
+import Tooltip from '@material-ui/core/Tooltip'
 import Typography from '@material-ui/core/Typography'
 
 const PageSkeleton = observer(class extends React.Component {
@@ -22,8 +23,10 @@ const PageSkeleton = observer(class extends React.Component {
     return (<React.Fragment>
       <AppBar>
         <Toolbar disableGutters={true} style={{ paddingRight: 8 }}>
-          <NavLink to={Path.home()} style={{ textDecoration: 'none' }}>
-            <img src={MircsLogo} alt='MIRCS Logo' style={{ height: 36, paddingLeft: 8, paddingRight: 12 }}/>
+          <NavLink to={Path.home()} style={{ textDecoration: 'none', padding: '0 20px' }}>
+            <Tooltip title='Return to Prototype Home' aria-label='Return to Prototype Home'>
+              <HomeIcon color='secondary' fontSize='large' />
+            </Tooltip>
           </NavLink>
 
           <Typography variant='title' color='inherit' noWrap>
