dbba79f82b25f3b983cf28c8349c91a6270fda3d
Shaun Johansen <shaunjohansen@gmail.com>
2021-07-18 16:36:13 -0300

    click logo to return home

--- react-app/src/components/PageSkeleton/PageSkeleton.js
+++ react-app/src/components/PageSkeleton/PageSkeleton.js
@@ -1,13 +1,15 @@
+import { NavLink } from 'react-router-dom'
 import { observer } from 'mobx-react'
 import AppBar from '@material-ui/core/AppBar'
 import AuthMenu from './AuthMenu'
 import Layout from '../../utils/Layout'
-import MircsLogo from '../../resources/mircs-logo-white.svg'
+import MircsLogoWhite from '../../resources/MircsLogoWhite'
 import PropTypes from 'prop-types'
 import React from 'react'
 import SignedInUser from '../../states/SignedInUser'
 import Toolbar from '@material-ui/core/Toolbar'
 import Typography from '@material-ui/core/Typography'
+import { Path } from '../../app/App'
 
 const PageSkeleton = observer(class extends React.Component {
   static propTypes = {
@@ -20,7 +22,9 @@ const PageSkeleton = observer(class extends React.Component {
     return (<React.Fragment>
       <AppBar>
         <Toolbar disableGutters={true} style={{ paddingRight: 8 }}>
-          <img src={MircsLogo} alt='MIRCS Logo' style={{ height: 36, paddingLeft: 8, paddingRight: 12 }}/>
+          <NavLink to={Path.home()} style={{ textDecoration: 'none' }}>
+            <MircsLogoWhite alt='MIRCS Logo' style={{ height: 36, paddingLeft: 8, paddingRight: 12 }}/>
+          </NavLink>
 
           <Typography variant='h4' color='inherit' noWrap>
             {title || 'MIRCS Geo-Genealogy'}
